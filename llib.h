688b64f0ed81669ef697780f606663c62f570a76
rohanpamera <153976467+rohanpamera@users.noreply.github.com>
2024-07-17 23:46:57 +0530

    Add files via upload

---
+++ llib.h
@@ -0,0 +1,19 @@
+#ifndef LLIB_H__
+#define LLIB_H__
+
+#include <llib/llib_memory.h>
+#include <llib/llib_algorithms.h>
+#include <llib/llib_memory.h>
+#include <llib/llib_io.h>
+#include <llib/llib_string.h>
+#include <llib/llib_utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // !LLIB_H__
